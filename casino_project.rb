cc97fd489e70bc112a0f57b4a96d21b203cdca1f
Ethan Clark <clark.ethan.r@gmail.com>
2020-08-27 18:39:52 -0600

    Seed Code

---
+++ casino_project.rb
@@ -0,0 +1,43 @@
+#Start game player has a name and an initial bankroll
+#Player can go to different games via menu
+  #Slots
+  #High / Low
+#Player places bet and wins / loses (hint: rand)
+#Player's bankroll goes up and down with wins and losses
+
+@money = 100
+require_relative 'card'
+require_relative 'deck'
+require_relative 'dice'
+def welcome
+  puts 'Welcome to the best Casino ever!'
+  puts 'What is your name?'
+  @user_name = gets.chomp
+end
+def menu
+  puts 'What would you like to do:'
+  puts '1) See your wallet total'
+  puts '2) Play Slots'
+  puts '3) Play Heads & Tails'
+  puts '4) Cashout and Checkout' #Exit
+  user_choice = gets.strip.to_i
+  if user_choice === 1
+    puts "Here is how much money you have now:"
+    money_total
+    menu
+  elsif user_choice === 2
+    play_slots
+    menu
+  elsif user_choice === 3
+    play_hilo
+    menu
+  elsif user_choice === 4
+    puts 'Better luck next time.'
+    exit
+  else 
+    puts 'Please choose a valid menu option'
+    menu
+  end
+end
+welcome
+menu 
