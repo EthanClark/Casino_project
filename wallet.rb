914b43638e29cf27c96ac0c5175cb5a5d191a427
Braxton McClellan <thebraxmc@gmail.com>
2020-09-01 20:36:20 -0600

    added bonus objectives

--- wallet.rb
+++ wallet.rb
@@ -1,4 +1,12 @@
-
-
 class Wallet
-  def wallet 
+attr_accessor :money
+  def initialize(@money)
+    money = 100
+    @money = money
+  end
+
+  def money_total
+     puts "Here is how much money you have now: $#{@money}"
+  end
+
+end
